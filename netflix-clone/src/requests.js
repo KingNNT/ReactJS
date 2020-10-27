1dcb33c486fd4924e8556f1d17cc1226d7d7b500
KingNNT <Dev.KingNNT@gmail.com>
2020-10-28 02:55:41 +0700

    Init

---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,9 @@
+const API_KEY = "0c52e0616d91a0ed7c284be9a70c339c";
+
+const request = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+
+}
