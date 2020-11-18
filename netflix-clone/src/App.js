f32bea20c5cff7260bab5e49818a7520832e286d
KingNNT <Dev.KingNNT@gmail.com>
2020-11-18 23:40:21 +0700

    Update

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,18 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Row from "./Row";
+import request from "./requests";
 
 function App() {
     return (
         <div className="App">
             <h1>Hello World</h1>
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={request.fetchNetflixOriginals}
+            />
+            <Row title="Trending Now" fetchUrl={request.fetchTrending} />
         </div>
     );
 }
