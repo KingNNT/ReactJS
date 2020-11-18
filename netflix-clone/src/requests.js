ec941778fbf2fe360bc3f2e0d7783b37bc182471
KingNNT <Dev.KingNNT@gmail.com>
2020-11-18 22:41:08 +0700

    Update

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -5,5 +5,10 @@ const request = {
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=25`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomancenMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
-}
+export default requests;
