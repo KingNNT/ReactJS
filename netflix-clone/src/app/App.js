84ad497a93b4c3a2da3e717f77734058ae59e4cc
KingNNT <Dev.KingNNT@gmail.com>
2020-11-19 13:04:39 +0700

    Update Row at Index

--- netflix-clone/src/app/App.js
+++ netflix-clone/src/app/App.js
@@ -6,12 +6,21 @@ import request from "../core/requests";
 function App() {
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1>Netflix Clone</h1>
             <Row
                 title="NETFLIX ORIGINALS"
                 fetchUrl={request.fetchNetflixOriginals}
             />
             <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+            <Row
+                title="Romancen Movies"
+                fetchUrl={request.fetchRomancenMovies}
+            />
+            <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
         </div>
     );
 }
